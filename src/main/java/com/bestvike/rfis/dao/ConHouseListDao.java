92218d68166df8ffa1fbfab366daee7a0018ee2e
yinxunyang <yinxunyang>
2019-12-02 15:30:10 +0800

    去掉多余代码

--- src/main/java/com/bestvike/rfis/dao/ConHouseListDao.java
+++
@@ -1,9 +0,0 @@
-package com.bestvike.rfis.dao;
-
-import com.bestvike.rfis.data.ConHouseList;
-import org.mybatis.mapper.common.BaseMapper;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ConHouseListDao extends BaseMapper<ConHouseList> {
-
-}
